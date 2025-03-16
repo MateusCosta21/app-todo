919c81229e6d61bd38baeb8f15cc5f5efeb08fac
Mateus <joao.santos@fabricainfo.com>
2025-03-16 11:36:47 -0300

    Update menu hamburguer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaSignOutAlt } from "react-icons/fa"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 interface NavbarProps {
   isAuthenticated: boolean;
@@ -10,6 +13,10 @@ interface NavbarProps {
 }
 
 const Navbar = ({ isAuthenticated, onLoginClick, onRegisterClick, onAddTaskClick, onLogout }: NavbarProps) => {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min");
+  }, []);
+
   const handleLogoutClick = () => {
     const confirmLogout = window.confirm("Você tem certeza que deseja sair?");
     if (confirmLogout) {
@@ -23,7 +30,15 @@ const Navbar = ({ isAuthenticated, onLoginClick, onRegisterClick, onAddTaskClick
         <Link to="/" className="navbar-brand">
           Todo App
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
@@ -31,13 +46,13 @@ const Navbar = ({ isAuthenticated, onLoginClick, onRegisterClick, onAddTaskClick
             {isAuthenticated ? (
               <>
                 <li className="nav-item">
-                  <button className="btn btn-primary" onClick={onAddTaskClick}>
+                  <button className="btn btn-primary me-2" onClick={onAddTaskClick}>
                     Cadastrar Tarefa
                   </button>
                 </li>
                 <li className="nav-item">
-                  <button className="btn btn-link nav-link" onClick={handleLogoutClick}>
-                    <FaSignOutAlt /> {/* Ícone de Logout */}
+                  <button className="btn btn-link nav-link text-danger" onClick={handleLogoutClick}>
+                    <FaSignOutAlt /> Sair
                   </button>
                 </li>
               </>
