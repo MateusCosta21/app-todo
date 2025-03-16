34e8cc8e8cb952fb544ae7662f2d291acbd8237b
Mateus <joao.santos@fabricainfo.com>
2025-03-16 13:16:37 -0300

    Add Run-dev.sh

---
+++ run-dev.sh
@@ -0,0 +1,11 @@
+cp .env.example .env
+if [ $? -ne 0 ]; then
+    echo "Erro ao copiar o arquivo .env.example para .env. Abortando."
+    exit 1
+fi
+echo "Subindo os containers do Docker..."
+docker-compose up -d --build
+if [ $? -ne 0 ]; then
+    echo "Erro ao subir os containers do Docker. Abortando."
+    exit 1
+fi
